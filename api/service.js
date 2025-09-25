57873db83a223a7509996fedf4236937da463827
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-09-25 17:36:56 +0100

    entryFix

--- api/service.js
+++ api/service.js
@@ -1,5 +1,5 @@
 import express from "express";
-import auhtRoutes from "./src/routes/authRoutes.js";
+import auhtRoutes from "../src/routes/authRoutes.js";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -14,7 +14,7 @@ app.use('/', async (_, res,) => {
 });
 app.use("/auth", auhtRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
